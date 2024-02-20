fee3f08fd8150e140cc017e7d03a326a99f39624
mmmmmam <144782356+mmmmmam@users.noreply.github.com>
2024-02-20 11:34:49 +0900

    Feat: 변경사항

--- src/main/java/com/shinhan/maahproject/restcontroller/ByCardDetailRestController.java
+++ src/main/java/com/shinhan/maahproject/restcontroller/ByCardDetailRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shinhan.maahproject.dto.AccountChangeDTO;
+import com.shinhan.maahproject.dto.AccountCheckDTO;
 import com.shinhan.maahproject.dto.ByCardBenefitsDTO;
 import com.shinhan.maahproject.dto.ByCardDetailDTO;
 import com.shinhan.maahproject.dto.MemberDTO;
