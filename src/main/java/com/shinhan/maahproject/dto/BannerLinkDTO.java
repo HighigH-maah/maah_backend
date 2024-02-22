eaf272d865b8bff8cbe660c4195a2450846b5363
ksweeni <77386337+ksweeni@users.noreply.github.com>
2024-02-22 17:36:25 +0900

    Feat: 메인 배너용 사용자 정보 DTO

---
+++ src/main/java/com/shinhan/maahproject/dto/BannerLinkDTO.java
@@ -0,0 +1,12 @@
+package com.shinhan.maahproject.dto;
+
+import lombok.Data;
+
+@Data
+public class BannerLinkDTO {
+	private int hiCardImageCode;
+	private String myHiNumber;
+	private String hiCardImageName;
+	private String memberId;
+
+}
